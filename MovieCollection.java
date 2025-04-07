c7694430d4319e095637c97945060ad070a5781a
korgul100 <chrisreed1972@gmail.com>
2025-04-07 13:03:33 -0400

    working towards a solution

--- MovieCollection.java
+++ MovieCollection.java
@@ -12,7 +12,9 @@ public class MovieCollection
     // adds a new movie to the movie collection
     public void addMovie(String title, int year, double rating)
     {
-        // Implement this method
+        Movie newMovie = new Movie(title, year, rating);
+        movies.add(newMovie);
+        System.out.println("Movie added successfully!");
     }
 
     // removes a movie from the movie collection
@@ -20,7 +22,23 @@ public class MovieCollection
     // return false if movie cannot be found
     public boolean removeMovie(String title)
     {
-        // Implement this method
+        int index = -1;
+        for (int i = 0; i < movies.size(); i++)
+        {
+            if (title.equals(movies.get(i).getTitle()))
+            {
+                index = i;
+            }
+        }
+        if (index != -1)
+        {
+            movies.remove(index);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     // search for a movie
@@ -28,19 +46,41 @@ public class MovieCollection
     // return null if movie is not in collection
     public Movie searchMovie(String title)
     {
-        // Implement this method
+        for (int i = 0; i < movies.size(); i++)
+        {
+            if (title.equals(movies.get(i)))
+            {
+                return movies.get(i);
+            }
+        }
         return null;
     }
 
     // displays all movies in the collection in order sorted by rating
     public void displayMovies()
     {
-        // Implement this method
+        sortMoviesByRating();
+        System.out.println(movies.size());
+        for (int i = 0; i < movies.size(); i++)
+        {
+            System.out.println(movies.get(i).getTitle() + ", " + movies.get(i).getYear() + ", " + movies.get(i).getRating());
+        }
     }
 
     // sorts movies by rating - use Bubblesort
     private void sortMoviesByRating()
     {
-        // Implement this method
+        for (int i = 0; i < this.movies.size() - 2; i++)
+        {
+            for (int j = this.movies.size() - 1; j > i; j--)
+            {
+                if (movies.get(j).getRating() < movies.get(j - 1).getRating())
+                {
+                    Movie temp = movies.get(j);
+                    movies.set(j, movies.get(j - 1));
+                    movies.set(j - 1, temp);
+                }
+            }
+        }
     }
 }
--- MovieManager.java
+++ MovieManager.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MovieManager
 {
     /* The main method to control program.
@@ -18,5 +20,71 @@ public class MovieManager
     Enter release year: 2010
     Enter rating (out of 10): 8.8
     Movie added successfully!
-    */ 
+    */
+
+    public static void main (String [] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        MovieCollection movieCollection = new MovieCollection();
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            writeOptions();
+            choice = Integer.parseInt(scanner.nextLine());
+            if (choice == 1)
+            {
+                System.out.print("Movie Title: ");
+                String title = scanner.nextLine();
+                System.out.print("Movie Year: ");
+                int year = Integer.parseInt(scanner.nextLine());
+                System.out.print("Movie Rating [0.0 - 10.0]: ");
+                double rating = Double.parseDouble(scanner.nextLine());
+                movieCollection.addMovie(title, year, rating);
+            }
+            else if (choice == 2)
+            {
+                System.out.println("Title of movie to be removed: ");
+                String title = scanner.nextLine();
+                if (movieCollection.removeMovie(title))
+                {
+                    System.out.println("Movie successfully removed.");
+                }
+                else
+                {
+                    System.out.println("Movie not removed - not in db!");
+                }
+            }
+            else if (choice == 3)
+            {
+                System.out.println("Title of movie to be searched: ");
+                String title = scanner.nextLine();
+                Movie movie = movieCollection.searchMovie(title);
+                if (movie != null)
+                {
+                    System.out.println("Movie in collection: " + movie.getTitle() + ", " + movie.getYear() + ", " + movie.getRating());
+                }
+                else
+                {
+                    System.out.println("Movie not in collection");
+                }
+            }
+            else if (choice == 4)
+            {
+                movieCollection.displayMovies();
+            }
+        }
+        scanner.close();
+    }
+
+    public static void writeOptions()
+    {
+        System.out.println("Welcome to the Movie Manager!");
+        System.out.println("1. Add a Movie");
+        System.out.println("2. Remove a Movie");
+        System.out.println("3. Search for a Movie");
+        System.out.println("4. Display all Movies");
+        System.out.println("5. Exit");
+        System.out.print("Enter a number: ");
+    }
 }
